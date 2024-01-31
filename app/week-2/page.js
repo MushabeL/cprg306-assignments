85e9b18d1049c15e4c07ac9a98b89d370ade9fc0
mushabe_b <lesleymushabe2@gmail.com>
2024-01-30 20:13:57 -0700

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306:Web Development 2- Assignments ]/h1>
+      <ul>
+        <li>
+          <a href="week-2">Week 2</a>
+        </li>
+      </u>
+    </main>
+  );
+}
+//mwah 
---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "nect/link";
+function StudentInfo() {
+  return (
+    <main>
+      <h1>Lesley Mushabe</h1>
+      <Link href="https://github.com/MushabeL">Lesley's Github</Link>
+    </main>
+  );
+}
