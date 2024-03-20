b41f8c0d52e4039fe63402f91a107fc041cc32fa
mushabe_b <lesleymushabe2@gmail.com>
2024-03-20 10:16:19 -0600

    removed files

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,2 +1,4 @@
 import { db } from "../_utils/firebase";
 import { collection, getDocs, addDoc, query } from "firebase/firestore";
+import { ShoppingListItem } from "../_models/shopping-list-item";
+import { useEffect } from "react";
--- week-6/item-list.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import Item from "./item";
-//import Items.json from "./items.json";
-
-//creating a state variable to store the items
-
-const [sortBy, setSortBy] = useState("name", "category");
-//sort the items two different ways to sort
